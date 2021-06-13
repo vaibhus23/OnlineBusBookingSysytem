3e83025211a6daa633b60748bfed6c7b00866def
sahil0472 <36255820+sahil0472@users.noreply.github.com>
2021-06-13 13:16:47 +0530

    update
    u

--- src/main/java/com/org/repository/IPassengerRepository.java
+++ src/main/java/com/org/repository/IPassengerRepository.java
@@ -3,7 +3,9 @@ package com.org.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.org.entities.Passenger;
-
+/*
+ * Passenger Repository
+ */
 public interface IPassengerRepository extends JpaRepository<Passenger, Integer> {
 
 }
