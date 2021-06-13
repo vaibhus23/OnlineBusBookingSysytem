3cc074e845bb197276e5332a08e5ef57152a6427
Rachit1998 <64678561+Rachit1998@users.noreply.github.com>
2021-06-13 13:52:39 +0530

    Updated repository

--- src/main/java/com/org/repository/IUserRepository.java
+++ src/main/java/com/org/repository/IUserRepository.java
@@ -14,13 +14,13 @@ import com.org.exceptions.InvalidUsernameException;
 public interface IUserRepository extends JpaRepository<User, String> {
 
 	@Query(value = "select * from User2 u where u.username=?1", nativeQuery = true)
-	public User getUserDetailsByUserName(String Username);
+	public User getUserDetailsByUserName(String username);
 
 	@Query(value = "delete from User2 u where u.username=?1", nativeQuery = true)
 	public void deleteUser(String username);
 	
 	@Modifying
 	@Query(value = "update User u set u.password=:password where u.username=:username")
-	public int updatePassoword(@Param("username") String username,@Param("password") String newPassword) throws InvalidUsernameException;
+	public int updatePassoword(@Param("username") String username, @Param("password") String newPassword) throws InvalidUsernameException;
 
 }
